231db398c5835b6b61d6c38794452ea698d384cd
Reem Ali <reem.ghany70@gmail.com>
2017-10-16 08:24:00 +0200

    some changes

--- src/Mv.java
+++ src/Mv.java
@@ -6,33 +6,47 @@ import java.util.Arrays;
 public class Mv extends Cmd {
 	public ArrayList<String> execute(ArrayList<String> args) {
 		ArrayList<String> ret = new ArrayList<String>();
-		if (new File(args.get(args.size() - 1)).isFile()) {
-			if (args.size() == 2) {
-				FileToFile(args);
-			} else {
-				ret.add("process can not be continued");
+		boolean check0 = checkDir(args.get(0)), check1 = checkDir(args.get(1));
+		if (args.size() == 2 && check0 == true
+				&& new File(args.get(0)).isFile()) {
+			if (check1 == false) {
+				File f1 = new File(args.get(1));
+				try {
+					f1.createNewFile();
+				} catch (Exception e) {
+				}
+
 			}
+			FileToFile(args);
+		} else if (args.size() == 2 && check0 == true && check1 == true
+				&& new File(args.get(0)).isFile()
+				&& new File(args.get(1)).isFile()) {
+			FileToFile(args);
+		} else if (args.size() == 2 && check0 == false) {
+			ret.add("process can not be continued");
 		} else {
-			ret.addAll(checkDir(args));
+			for (int i = 0; i < args.size(); i++) {
+				if (checkDir(args.get(i)) == false)
+					args.set(i, null);
+			}
+			if (args.get(args.size() - 1) == null)
+				new Mkdir().execute(new ArrayList<String>(Arrays.asList(args
+						.get(args.size() - 1))));
 			recurseOnDir(args.get(args.size() - 1), args);
 		}
 		return ret;
 	}
 
-	public ArrayList<String> checkDir(ArrayList<String> args) {
-		ArrayList<String> ret = new ArrayList<String>();
-		for (int i = 0; i < args.size(); i++) {
-			File f = new File(args.get(i));
+	public Boolean checkDir(String args) {
+		File f = new File(args);
+		if (!f.exists()) {
+			args = CLI.workingDirectory + '/' + args;
+			f = new File(args);
 			if (!f.exists()) {
-				args.set(i, CLI.workingDirectory + '/' + args.get(i));
-				f = new File(args.get(i));
-				if (!f.exists()) {
-					ret.add("there is no such directory :" + args.get(i));
-					args.set(i, null);
-				}
+				return false;
 			}
 		}
-		return ret;
+		return true;
 	}
 
 	public void recurseOnDir(String f, ArrayList<String> args) {
@@ -46,13 +60,13 @@ public class Mv extends Cmd {
 			}
 			String tmpWorkingDir = CLI.workingDirectory;
 			CLI.workingDirectory = f;
+			CLI.workingDirectory = tmpWorkingDir;
 			new Mkdir().execute(new ArrayList<String>(Arrays.asList(dir)));
 			recurseOnDir(
 					f + "/" + dir,
 					new ArrayList<String>(new Ls()
 							.execute(new ArrayList<String>(Arrays.asList(dir)))));
 
-			CLI.workingDirectory = tmpWorkingDir;
 		}
 	}
 
@@ -69,11 +83,23 @@ public class Mv extends Cmd {
 
 	public ArrayList<String> FileToFile(ArrayList<String> args) {
 		File f = new File(args.get(0)), f1 = new File(args.get(1));
+		System.out.println("reem d5lt1");
 		if (!f.exists()) {
 			args.set(0, CLI.workingDirectory + '/' + args.get(0));
 		}
 		if (!f1.exists()) {
-			args.set(1, CLI.workingDirectory + '/' + args.get(1));
+			if (args.get(1).indexOf("/home") == -1)
+				args.set(1, CLI.workingDirectory + '/' + args.get(1));
+			System.out.println("reem d5lt2");
+			f1 = new File(args.get(1));
+			if (!f1.exists()) {
+				try {
+					f1.createNewFile();
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+
+			}
 		}
 		ArrayList<String> ret = new ArrayList<String>();
 		String checker = checkFile(args);
